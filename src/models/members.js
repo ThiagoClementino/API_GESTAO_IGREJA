848aedd44418ff59cc766f577cd44241c978e662
ThiagoCe01 <thidf57@gmail.com>
2024-07-10 16:01:02 -0300

    mudança no eschema:: campo matricula

--- src/models/members.js
+++ src/models/members.js
@@ -15,29 +15,29 @@ const generateUniqueId = async () => {
 
 
 
-const dataMatricula = () => {
-  const data = new Date();
-  const dia = data.getDate();
-  const mes = data.getMonth() + 1;
-  const ano = data.getFullYear();
-  return `${dia.toString().padStart(2, '0')}-${mes.toString().padStart(2, '0')}-${ano}`;
-  
-};
+  const dataMatricula = () => {
+    const data = new Date();
+    const dia = data.getDate();
+    const mes = data.getMonth() + 1;
+    const ano = data.getFullYear();
+    return `${dia.toString().padStart(2, '0')}-${mes.toString().padStart(2, '0')}-${ano}`;
+    
+  };
 const membersSchema = new Schema({
  _id: {
     type: String,
     default: async () => await generateUniqueId()
   },
-  matricula: {
-    type: String,
-    default: async () => await dataMatricula(),
-    required: false,
-    unique: true,
-    trim: true,
-  },
+  // matricula: {
+  //   type: String,
+  //   required: false,
+  //   unique: true,
+  //   trim: true,
+  // },
   datacriacao: {
     type: String,
     required: false,
+    default: async () => await dataMatricula()
   },
   name: {
     type: String,
@@ -52,7 +52,7 @@ const membersSchema = new Schema({
     required: false,
   },
   dateBirth: {
-    type: String,
+    type: Date,
     required: false,
   },
   sex: {
