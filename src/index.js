46e7d552b7ef0eab869fa4027bed4c09d7389f49
ThiagoClementino <thidf57@gmail.com>
2025-07-18 02:14:32 -0300

    ajustes da api

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 import express from "express";
 import connectDatabase from "./database/database.js";
-import routes from './routes.js'
-import cors from "cors"
+import routes from "./routes.js";
+import cors from "cors";
 const PORT = 3060;
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-
-
+app.get("/", (req, res) => {
+  res.json({
+    message: "API funcionando!",
+  });
+});
 
 connectDatabase()
   .then(() => {
-    app.listen(PORT, () => console.log(`Servidor e Banco de dados rodando no endereço: http://localhost:${PORT}`)
+    app.listen(PORT, () =>
+      console.log(
+        `Servidor e Banco de dados rodando no endereço: http://localhost:${PORT}`
+      )
     );
   })
-  .catch((erro) => console.log("Banco de dados não conectado", ));
-
-
+  .catch((erro) => console.log("Banco de dados não conectado"));
--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,29 @@
 import { Router } from "express";
-import { getMember, getMemberschek, getMembers, postMembers, deleteMembers, putMembers, getfinance, postfinance, deletefinance, putfinance,   } from "./controllers/UserController.js";
-import cors from 'cors'
+import {
+  getMember,
+  getMemberschek,
+  getMembers,
+  postMembers,
+  deleteMembers,
+  putMembers,
+  getfinance,
+  postfinance,
+  deletefinance,
+  putfinance,
+} from "./controllers/UserController.js";
+import cors from "cors";
 const routes = Router();
 
-routes.get('/membros/ok', getMemberschek);
-routes.get('/membros/:id', getMember);
-routes.get('/membros', getMembers);
-routes.post('/membros', cors(), postMembers);
-routes.delete('/membros/:id', deleteMembers);
-routes.put('/membros/:id', putMembers);
+routes.get("/membros/ok", getMemberschek);
+routes.get("/membros/:id", getMember);
+routes.get("/membros", getMembers);
+routes.post("/membros", cors(), postMembers);
+routes.delete("/membros/:id", deleteMembers);
+routes.put("/membros/:id", putMembers);
 
-
-
-routes.get('/finance', getfinance);
-routes.post('/finance', cors(), postfinance);
-routes.delete('/finance/:id', deletefinance);
-routes.put('/finance/:id', putfinance);
+routes.get("/finance", getfinance);
+routes.post("/finance", cors(), postfinance);
+routes.delete("/finance/:id", deletefinance);
+routes.put("/finance/:id", putfinance);
 
 export default routes;
